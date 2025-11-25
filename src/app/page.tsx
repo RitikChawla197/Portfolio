6efb1f2fcc83a0836da6993602f3dc8aca14b7af
Ritik Chawla <64843293+RitikChawla197@users.noreply.github.com>
2025-11-25 13:04:50 +0530

    Implement loading screen in Home component
    Added a loading screen that displays while content is loading.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import AboutSection from '@/components/about-section'
 import ContactSection from '@/components/contact-section'
 import ExperienceSection from '@/components/experience-section'
@@ -6,22 +8,34 @@ import HeroSection from '@/components/hero-section'
 import ProjectsSection from '@/components/projects-section'
 import SectionDivider from '@/components/section-divider'
 import { ThemeToggle } from '@/components/theme-toggle'
+import LoadingScreen from '@/components/LoadingScreen'  
+import { useState } from 'react'
 
 export default function Home() {
-  return (
+
+   const [loading, setLoading] = useState(true);
+
+ return (
     <>
-      <div className="max-w-3xl mx-auto flex flex-col items-center">
-        <Header />
-        <HeroSection />
-        <AboutSection />
-        <SectionDivider />
-        <ExperienceSection />
-        <SectionDivider />
-        <ProjectsSection />
-        <ContactSection />
-      </div>
-      {/* Desktop: Theme Toggle */}
-      <ThemeToggle className="bg-background hidden sm:fixed sm:bottom-8 sm:right-8 sm:flex" />
+      {loading ? (
+        <LoadingScreen onFinish={() => setLoading(false)} />
+      ) : (
+        <>
+          <div className="max-w-3xl mx-auto flex flex-col items-center">
+            <Header />
+            <HeroSection />
+            <AboutSection />
+            <SectionDivider />
+            <ExperienceSection />
+            <SectionDivider />
+            <ProjectsSection />
+            <ContactSection />
+          </div>
+
+          {/* Desktop: Theme Toggle */}
+          <ThemeToggle className="bg-background hidden sm:fixed sm:bottom-8 sm:right-8 sm:flex" />
+        </>
+      )}
     </>
-  )
+  );
 }
